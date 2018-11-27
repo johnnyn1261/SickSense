1d19a852c7cb90a70e1816d9de6f7ed205ec454d
johnnyn1261 <johnnyn1261@gmail.com>
2018-11-26 19:04:50 -0500

    Added support.php file

---
+++ support.php
@@ -0,0 +1,35 @@
+<?php
+
+function generatePage($body) {
+    $page = <<<EOPAGE
+    <!DOCTYPE html>
+    <html lang="en" dir="ltr">
+      <head>
+        <meta charset="utf-8">
+        <title>SickSense</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <meta name="keywords" content="html5, css3, bootstrap, php, js, firebase">
+        <meta name="author" content="Van-Nhan, Jeff, Israel, Kasim, Enock">
+
+        <link rel="shortcut icon" type="image/png" href="sickSense.png">
+
+        <!-- CSS Stylesheets -->
+        <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/bootstrap.css">
+      </head>
+      <body>
+        $body
+      </body>
+
+      <!-- JavaScripts -->
+      <script src="js/jquery.js"></script>
+      <script src="js/bootstrap.js"></script>
+      <script src="js/script.js"></script>
+    </html>
+EOPAGE;
+
+    return $page;
+}
+?>
